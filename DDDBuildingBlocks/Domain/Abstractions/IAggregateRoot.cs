b020aa5f49cc7a3ea36ecdcac6bd818e5d78442d
Marcell Toth <marcell@marcelltoth.net>
2019-04-01 12:34:21 +0200

    Add IAggregateRoot interface

---
+++ DDDBuildingBlocks/Domain/Abstractions/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+namespace MarcellToth.DDDBuildingBlocks.Domain.Abstractions
+{
+    /// <summary>
+    ///     Marker interface for DDD Aggregate Roots
+    /// </summary>
+    public interface IAggregateRoot
+    {
+        
+    }
+}
