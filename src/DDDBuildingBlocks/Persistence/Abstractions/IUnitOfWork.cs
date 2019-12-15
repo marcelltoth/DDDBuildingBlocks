9eb8ab111af7b93e6baba5d86afefda3f1cc2594
Marcell Toth <marcell@marcelltoth.net>
2019-12-15 12:08:51 +0100

    Make IUnitOfWork IDisposable

--- src/DDDBuildingBlocks/Persistence/Abstractions/IUnitOfWork.cs
+++ src/DDDBuildingBlocks/Persistence/Abstractions/IUnitOfWork.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,12 +7,12 @@ namespace MarcellToth.DDDBuildingBlocks.Persistence.Abstractions
     /// <summary>
     ///     Interface for a Unit of Work pattern. A unit of work tracks changes to entities it has loaded, and persists them all at once in a single transaction.
     /// </summary>
-    /// <seealso cref="SaveEntitiesAsync"/>
-    public interface IUnitOfWork
+    /// <seealso cref="SaveChangesAsync"/>
+    public interface IUnitOfWork : IDisposable
     {
         /// <summary>
         ///     Persists all changes tracked by this <see cref="IUnitOfWork"/> as a single transaction. Either all changes need to be persisted or none.
         /// </summary>
-        Task SaveEntitiesAsync(CancellationToken ct = default);
+        Task SaveChangesAsync(CancellationToken ct = default);
     }
 }
